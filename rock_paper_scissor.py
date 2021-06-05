a208ce8d764509248afcf23201337386531b7ccc
Sandesh Prabhu <77835063+sandesh-prabhu@users.noreply.github.com>
2021-06-05 16:34:46 +0530

    Update rock_paper_scissor.py

--- rock_paper_scissor.py
+++ rock_paper_scissor.py
@@ -1,35 +1,67 @@
+#import library
+from tkinter import *
 import random
-def win(a,b):
-    print("YOU WIN!")
-    print("Your choice:",a,"\nComputer's choice:",b)
-def lose(a,b):
-    print("YOU LOST!")
-    print("Your choice:",a,"\nComputer's choice:",b)
-while 1:
-    print("\n--------------------------\nROCK - PAPER - SCISSOR\nMake your choice:")
-    data=(input("Rock / Paper / Scissor (Enter nothing to quit):"))
-    if data =="":
-        print("Have a good day!")
-        break
-    data=data.lower()
-    if data not in ["rock","paper","scissor"]:
-        print("Please provide valid input.")
-        continue
-    rnum=random.randint(0,2)
-    arr=["rock","paper","scissor"]
-    if data == arr[rnum]:
-        print("It's a TIE")
-        print("Your choice:",data,"\nComputer's choice:",arr[rnum])
-        continue
-    if data=="rock":
-        if rnum==1:
-            lose(data,arr[rnum])
-        else: win(data,arr[rnum])
-    elif data=="paper":
-        if rnum==2:
-            lose(data,arr[rnum])
-        else: win(data,arr[rnum])
+
+#initialize window
+root = Tk()
+root.geometry('400x400')
+root.resizable(0,0)
+root.title('Game - Rock, Paper, Scissor')
+root.config(bg ='seashell3')
+
+#heading
+Label(root, text = 'Rock, Paper , Scissor' , font='arial 20 bold', bg = 'seashell2').pack()
+
+#user choice
+user_take= StringVar()
+Label(root, text = 'Choose any one: rock, paper ,scissor' , font='arial 15 bold', bg = 'seashell2').place(x=15 , y = 80)
+Entry(root, font = 'arial 15', textvariable = user_take , bg = 'antiquewhite2').place(x=90 , y = 130)
+
+#function to play
+Result = StringVar()
+
+def play():
+    user_pick = user_take.get().lower()
+
+    #computer choice
+    comp_pick = random.randint(1,3)
+    if comp_pick == 1:
+        comp_pick = 'rock'
+    elif comp_pick ==2:
+        comp_pick = 'paper'
     else:
-        if rnum==0:
-            lose(data,arr[rnum])
-        else: win(data,arr[rnum])
+        comp_pick = 'scissor'
+    
+    if user_pick == comp_pick:
+        Result.set('TIE!! you both chose same')
+    elif user_pick == 'rock' and comp_pick == 'paper':
+        Result.set('YOU LOSE!! computer chose paper')
+    elif user_pick == 'rock' and comp_pick == 'scissor':
+        Result.set('YOU WIN!! computer chose scissor')
+    elif user_pick == 'paper' and comp_pick == 'scissor':
+        Result.set('YOU LOSE!! computer chose scissor')
+    elif user_pick == 'paper' and comp_pick == 'rock':
+        Result.set('YOU WIN!! computer chose rock')
+    elif user_pick == 'scissor' and comp_pick == 'rock':
+        Result.set('YOU LOSE!! computer chose rock')
+    elif user_pick == 'scissor' and comp_pick == 'paper':
+        Result.set('YOU WIN!! computer chose paper')
+    else:
+        Result.set('Invalid choice: Choose any one --> rock, paper, scissor')
+
+#fun to reset
+def Reset():
+    Result.set("") 
+    user_take.set("")
+
+#fun to exit
+def Exit():
+    root.destroy()
+
+#button
+Entry(root, font = 'arial 10 bold', textvariable = Result, bg ='antiquewhite2',width = 50,).place(x=25, y = 250)
+Button(root, font = 'arial 13 bold', text = 'PLAY'  ,padx =5,bg ='seashell4' ,command = play).place(x=150,y=190)
+Button(root, font = 'arial 13 bold', text = 'RESET'  ,padx =5,bg ='seashell4' ,command = Reset).place(x=70,y=310)
+Button(root, font = 'arial 13 bold', text = 'EXIT'  ,padx =5,bg ='seashell4' ,command = Exit).place(x=230,y=310)
+
+root.mainloop()
